ae175f205a8e765cd4a8718f2e513fe8b3517552
rupeshjoshi <rupesh.joshi@berylsystems.com>
2015-11-04 16:05:19 +0530

    railsqweries

--- blog/app/controllers/home_controller.rb
+++ blog/app/controllers/home_controller.rb
@@ -1,8 +1,19 @@
 class HomeController < ApplicationController
 	def index
-      @user=User.all
-     
+      @user = User.order(name: :asc)
 
-    
+
+	end
+
+	def show
+      @user = User.find(params[:id])
+      @a = User.find([1,5])
+      #@takeuser=User.take(2)
+      @takeuser = User.take
+      @firstuser = User.first(3)
+      @lastuser = User.last
+      #@lastuser = User.last(3)
+      @find_byuser = User.find_by name:'Saurav'
 	end
 end
+
